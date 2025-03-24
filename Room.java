c4439554bb8c4b1df123964081f3b635b10bb88d
tjaung <jaungt@gmail.com>
2025-03-24 19:42:39 -0400

    have basic room class

---
+++ Room.java
@@ -0,0 +1,91 @@
+/**
+ * Class for Room.
+ */
+public class Room {
+    private String roomName;
+    private int roomNumber;
+    private String description;
+    private int N;
+    private int S;
+    private int E;
+    private int W;
+    private Puzzle puzzle;
+    private Item items;
+    private Fixture fixtures;
+    private String picture;
+
+    /**
+     * Constructor for Room. Takes in a lot of parameters to construct it. 
+     */
+    public Room(
+        String roomName, 
+        int: roomNumber, 
+        String: description, 
+        int: N, 
+        int: S, 
+        int: E, 
+        int: W, 
+        Puzzle: puzzle,
+        Item: items, 
+        Fixture: fixtures, 
+        String picture) {
+            
+        // Check parameters
+        this.checkParameters(roomName, roomNumber, description, N, S, E, W, puzzle, items, fixtures, picture)
+        this.roomName = roomName;
+        this.description = description;
+        this.N = N;
+        this.S = S;
+        this.E = E;
+        this.W = W;
+        this.puzzle = puzzle;
+        this.items = items;
+        this.picture = picture;
+    }
+
+    /**
+     * Getter for the room description.
+     * 
+     * @return desc - string of the room desc.
+     */
+    public String getRoomDescription() {
+        return description;
+    }    
+
+    /**
+     * Getter for North direction.
+     * 
+     * @return N - int of N room
+     */
+    public int getN() {
+        return N;
+    }
+
+    /**
+     * Getter for South direction.
+     * 
+     * @return S - int of S room
+     */
+    public int getS() {
+        return S;
+    }
+
+    /**
+     * Getter for East direction.
+     * 
+     * @return E - int of E room
+     */
+    public int getE() {
+        return E;
+    }
+
+    /**
+     * Getter for West direction.
+     * 
+     * @return W - int of W room
+     */
+    public int getW() {
+        return W;
+    }
+    
+}
