9198c17e1ce573769a9f78e9129defd550ed398f
DanielRJilek <jilek.d@northeastern.edu>
2025-03-25 16:04:08 -0400

    Update Puzzle.java
    added constructor for puzzle

--- gameelements/Puzzle.java
+++ gameelements/Puzzle.java
@@ -11,6 +11,19 @@ public class Puzzle {
   private Object target;
   private String picture;
 
+  public Puzzle(boolean affects_target, boolean affects_player, boolean active, Item solution, int value,
+                String effects, String description, Object target, String picture) {
+    this.affects_target = affects_target;
+    this.affects_target = affects_target;
+    this.active = active;
+    this.solution = solution;
+    this.value = value;
+    this.effects = effects;
+    this.description = description;
+    this.target = target;
+    this.picture = picture;
+  }
+
   public boolean isActive() {
     return active;
   }
