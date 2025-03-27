3d81ab0a812c04ade551a513599ec76470666e60
Daniel <jilek.d@northeastern.edu>
2025-03-27 14:33:17 -0400

    added enginedriver package

---
+++ enginedriver/GameEngineApp.java
@@ -0,0 +1,15 @@
+package enginedriver;
+
+import java.io.IOException;
+
+public class GameEngineApp {
+
+  public GameEngineApp(String gameFileName, Readable source, Appendable output) {
+
+  }
+
+  public void start() {
+    // Should create a new GameModel and new Controller
+    // Then tell controller to go
+  }
+}
