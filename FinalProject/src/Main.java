352f4406704ce3cefe073c5bdaa10b7e13b6fc55
Daniel <jilek.d@northeastern.edu>
2025-03-28 14:08:28 -0400

    Fixed absolute file path

--- FinalProject/src/Main.java
+++ FinalProject/src/Main.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.StringReader;
 
 import enginedriver.GameEngineApp;
@@ -10,12 +9,12 @@ public class Main {
     // smoke tests - first send synthetic data via a string
     String s = "Sir Mix-A-Lot\nT NOTEBOOK\nN\nT HAIR CLIPPERS\nT KEY\nD NOTEBOOK\nQuit";
     BufferedReader stringReader = new BufferedReader(new StringReader(s));
-    GameEngineApp gameEngineApp = new GameEngineApp("../FinalProject/src/resources/align_quest_game_elements.json", stringReader, System.out);
+    GameEngineApp gameEngineApp = new GameEngineApp("../resources/dummy.json", stringReader, System.out);
     gameEngineApp.start();
 
 
     // Next, comment the above and uncomment this to do some ad-hoc testing by hand via System.in
-//     GameEngineApp gameEngineApp = new GameEngineApp("../FinalProject/src/resources/align_quest_game_elements.json", new InputStreamReader(System.in), System.out);
-//     gameEngineApp.start();
+    // GameEngineApp gameEngineApp = new GameEngineApp("./resources/museum.json", new InputStreamReader(System.in), System.out);
+    // gameEngineApp.start();
   }
 }
