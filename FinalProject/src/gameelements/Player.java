c07486e79e13b03e139d2217e3e749a9e3066f7e
Roh-K-0 <kumar.rohan1@northeastern.edu>
2025-03-29 22:46:43 -0400

    Update Player.java
    add comments

--- FinalProject/src/gameelements/Player.java
+++ FinalProject/src/gameelements/Player.java
@@ -7,16 +7,22 @@ import java.util.List;
 import model.Parser;
 import model.ReverseParser;
 
+/**
+ * The Player class
+ */
 public class Player {
-  private String name;
-  private int score;
-  private int health;
-  private Room currentRoom;
-  private Inventory inventory;
-  private List<Puzzle> solvedPuzzles;
-  private List<Monster> defeatedMonsters;
+  
+  private String name;              
+  private int score;             
+  private int health;               
+  private Room currentRoom;         
+  private Inventory inventory;     
+  private List<Puzzle> solvedPuzzles;  
+  private List<Monster> defeatedMonsters; 
 
-
+  /**
+   * Constructor. 
+   */
   public Player() {
     this.health = 100;
     this.inventory = new Inventory(10, 0, "./");
@@ -24,26 +30,54 @@ public class Player {
     this.defeatedMonsters = new ArrayList<>();
   }
 
+  /**
+   * Gets the name of the player.
+   * 
+   * @return The name of the player.
+   */
   public String getName() {
     return this.name;
   }
 
+  /**
+   * Gets the score of the player.
+   * 
+   * @return The score of the player.
+   */
   public int getScore() {
     return this.score;
   }
 
+  /**
+   * Gets the current room the player is in.
+   * 
+   * @return The current room.
+   */
   public Room getCurrentRoom() {
     return currentRoom;
   }
 
+  /**
+   * Gets the health of the player.
+   * 
+   * @return The health of the player.
+   */
   public int getHealth() {
     return health;
   }
 
+  /**
+   * Gets the inventory of the player.
+   * 
+   * @return The player's inventory.
+   */
   public Inventory getInventory() {
     return inventory;
   }
 
+  /**
+   * Sets the player's inventory with new parameters and adds items to it.
+   */
   public void setInventory(int maxWeight, int currentWeight, String path, List<Item> itemList) {
     this.inventory = new Inventory(maxWeight, currentWeight, path);
     for(Item item : itemList) {
@@ -51,39 +85,74 @@ public class Player {
     }
   }
 
+  /**
+   * Sets the name of the player.
+   * 
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Sets the score of the player.
+   * 
+   */
   public void setScore(int score) {
     this.score = score;
   }
 
+  /**
+   * Sets the health of the player.
+   * 
+   */
   public void setHealth(int health) {
     this.health = health;
   }
 
+  /**
+   * Sets the current room the player is in.
+   * 
+   */
   public void setCurrentRoom(Room currentRoom) {
     this.currentRoom = currentRoom;
   }
 
+  /**
+   * Sets the inventory of the player.
+   * 
+   */
   public void setInventory(Inventory inventory) {
     this.inventory = inventory;
   }
 
+  /**
+   * Converts the player object to a JSON string.
+   * 
+   */
   public String parsePlayerToJSON() {
     String playerStr = ReverseParser.readPlayer(this);
     return playerStr;
   }
 
+  /**
+   * Adds a monster to the list of defeated monsters.
+   * 
+   */
   public void addMonster(Monster monster) {
     this.defeatedMonsters.add(monster);
   }
 
+  /**
+   * Adds a puzzle to the list of solved puzzles.
+   * 
+   */
   public void addPuzzle(Puzzle puzzle) {
     this.solvedPuzzles.add(puzzle);
   }
 
+  /**
+   * Calculates the player's score
+   */
   public void calculateScore() {
     int score = 0;
     for (Item i : this.getInventory().getItems()) {
@@ -98,6 +167,9 @@ public class Player {
     this.setScore(score);
   }
 
+  /**
+   * Returns a string describing the player's health status.
+   */
   public String toString() {
     if (this.health <= 0 )
       return "You fell asleep!";
