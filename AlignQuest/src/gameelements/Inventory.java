37a25599bcc54051353890ba8b8b293bb3da8479
Daniel <jilek.d@northeastern.edu>
2025-04-01 16:37:11 -0400

    Fixed comments and code clean up for Inventory

--- AlignQuest/src/gameelements/Inventory.java
+++ AlignQuest/src/gameelements/Inventory.java
@@ -6,60 +6,57 @@ import java.util.List;
 import model.ReverseParser;
 
 /**
- * Class for a Inventory concrete Object.
+ * Class for an Inventory concrete Object.
  */
 public class Inventory {
     // Instance variables to store the inventory's attributes
     private final int maxWeight;  // The maximum weight
-    private int currWeight;       // The current weight of the items 
+    private int currWeight;       // The current weight of the items
+    private String picture;        // The path to an image
     private final List<Item> items;  // A list to hold Item objects
-    private String picture;        // The path to an image 
 
     /**
-   * Constructor for Inventory. 
-   *
-   * @param maxWeight - int of the inventory's max weight from 0 to inf.
-   * @param currWeight - int of the inventory's curr weight. 
-   * @param picture - a string path to the corresponding asset.
-   */
-    public Inventory(int maxWeight, int currWeight, String path) {
+     * Constructor for Inventory.
+     * @param maxWeight - int of the inventory's max weight from 0 to inf.
+     * @param currWeight - int of the inventory's curr weight.
+     * @param picture - a string path to the corresponding asset.
+     */
+    public Inventory(int maxWeight, int currWeight, String picture) {
         this.maxWeight = maxWeight;     
         this.currWeight = currWeight;    
         this.items = new ArrayList<>();  
-        this.picture = path;           
+        this.picture = picture;
     }
 
     /**
-   * Getter for current weight.
-   *
-   * @return current weight.
-   */
+     * Getter for current weight.
+     * @return current weight.
+     */
     public int getCurrWeight() {
         return currWeight;  
     }
 
 
-     /**
-   * Getter for max weight.
-   *
-   * @return max weight.
-   */   
+    /**
+     * Getter for maxWeight.
+     * @return  maxWeight.
+     */
     public int getMaxWeight() {
         return maxWeight;
     }
 
     /**
-   * Getter for items.
-   *
-   * @return items in a list.
-   */   
+     * Getter for items.
+     * @return  the list of items in the Inventory.
+     */
     public List<Item> getItems() {
         return items; 
     }
 
     /**
-   * addItem() adds a item to the inventory 
-   */  
+     * Adds an item to the Inventory.
+     * @param item  the item to be added.
+     */
     public void addItem(Item item) {
         if (this.currWeight + (item).getWeight() > this.maxWeight) {
             throw new IllegalArgumentException("You don't have room in your inventory.\n");
@@ -69,9 +66,10 @@ public class Inventory {
         }
     }
 
-      /**
-   * drops a item to the inventory 
-   */  
+    /**
+     * Drops an item from the Inventory.
+     * @param item  the item to be dropped.
+     */
     public void dropItem(Item item) {
         if (this.getItems().contains(item)) {
             this.items.remove(item);
@@ -81,11 +79,11 @@ public class Inventory {
         }
     }
 
-     /**
-   * Checks if item is in a list.
-   *
-   * @return boolean.
-   */      
+    /**
+     * Checks to see if an item is in the Inventory.
+     * @param item  an item that may or may not be in the Inventory.
+     * @return  boolean true if item is in Inventory.
+     */
     public boolean hasItem(Item item) {
         for (IRoomElement i : this.items) {
             if (i.getName().equalsIgnoreCase(item.getName())) {
@@ -96,10 +94,10 @@ public class Inventory {
     }
 
     /**
-   * gets an Item from a room.
-   *
-   * @return Item.
-   */      
+     * Getter for an Item in the Inventory by name.
+     * @param itemName  the item's name.
+     * @return  the item object itself.
+     */
     public Item getItem(String itemName) {
         if(itemName.isEmpty()) {
             return null;
@@ -112,16 +110,18 @@ public class Inventory {
         throw new IllegalArgumentException("You don't have a ".concat(itemName).concat(" in your inventory.\n"));
     }
 
-   /**
-   * returns the picture path.
-   */      
+    /**
+     * Getter for picture.
+     * @return  picture.
+     */
     public String getPicture() {
         return picture;
     }
 
     /**
-   * parses json
-   */     
+     * TO DO. SHOULD NOT BE IN THIS CLASS.
+     * @return
+     */
     public String parseInventoryToJSON() {
         return ReverseParser.readInventory(this);
     }
