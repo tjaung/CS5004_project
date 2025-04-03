5080e9f0c2e8dc0e63f58cfc71885531db62fa88
Daniel <jilek.d@northeastern.edu>
2025-04-02 20:32:23 -0400

    Added resources, created sample layout, and dummy north button

--- AlignQuest/src/view/GameView.java
+++ AlignQuest/src/view/GameView.java
@@ -7,17 +7,75 @@ import javax.swing.*;
 import controller.VisualController;
 
 public class GameView extends JFrame {
-  private JButton x;
+  private JButton north, south, east, west;
+  private JButton inventory;
+  private JButton look;
+  private JButton examine;
+  private JButton use;
+  private JButton take;
+  private JButton drop;
+  private JButton answer;
+  private JPanel imagePanel;
+  private JPanel optionPanel;
+  private JPanel movePanel;
+  private JPanel actionPanel;
 
   public GameView(String caption) {
+    // setup frame
     super(caption);
-    setSize(200, 125);
-    setLocation(200,200);
+
+    setSize(400, 400);
+    setLocation(400,400);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    // Setup panels
+    this.imagePanel = new JPanel();
+    this.imagePanel.setBounds(0,0,400,200);
+    this.optionPanel = new JPanel();
+    this.optionPanel.setBounds(0,200,400,200);
+    this.movePanel = new JPanel();
+    this.movePanel.setBounds(0,0,200,200);
+    this.actionPanel = new JPanel();
+    this.actionPanel.setBounds(200,0,200,200);
 
-    setLayout(new GridLayout(0,2));
+    this.setLayout(new GridLayout(2, 0));
+    this.add(this.imagePanel);
+    this.add(this.optionPanel);
 
+    // add butttons
+    this.north = new JButton("");
+    try {
+      ImageIcon northIcon = new ImageIcon("../AlignQuest/resources/resources/images/north.png");
+      this.north.setIcon(northIcon);
+      this.north.setBorderPainted(false);
+      this.north.setContentAreaFilled(false);
+      this.north.setFocusPainted(false);
+    }
+    catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    this.north.setBounds(70, 10, 80, 80);
+    this.south = new JButton("");
+    this.south.setBounds(70, 70, 50, 50);
+    this.east = new JButton("");
+    this.east.setBounds(130, 35, 50, 50);
+    this.west = new JButton("");
+    this.west.setBounds(10, 35, 50, 50);
+
+    /*this.optionPanel.setLayout(new GridLayout(0,2));
+    this.optionPanel.add(this.movePanel);
+    this.optionPanel.add(this.actionPanel);*/
+
+    /*this.movePanel.setLayout(new GridLayout(2,2));
+    this.actionPanel.setLayout(new GridLayout(2, 4));*/
+
+    this.optionPanel.setLayout(null);
+    this.optionPanel.add(this.north);
+    this.optionPanel.add(this.south);
+    this.optionPanel.add(this.east);
+    this.optionPanel.add(this.west);
+
+    // build menu bar
     this.setJMenuBar(this.buildMenu());
   }
 
@@ -49,5 +107,7 @@ public class GameView extends JFrame {
 
   public void display() {
     this.setVisible(true);
+    //this.imagePanel.setVisible(true);
+    //this.optionPanel.setVisible(true);
   }
 }
