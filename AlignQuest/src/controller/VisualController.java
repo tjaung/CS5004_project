ffc0677d31e673a6b481fca4aa0b798bcbca1cd6
Daniel <jilek.d@northeastern.edu>
2025-04-08 16:04:15 -0400

    take no longer throws an error on cancel

--- AlignQuest/src/controller/VisualController.java
+++ AlignQuest/src/controller/VisualController.java
@@ -72,12 +72,14 @@ public class VisualController implements ActionListener {
       case "T":
         try {
           IRoomElement takeItem = PopUp.openListPopUp(this.model.getRoomModel().getCurrentRoom().getItems());
+          if (takeItem != null) {
           this.model.takeItem((Item) takeItem);
           PopUp.confirmPopUp("You have added " + takeItem.getName() + " to your inventory.");
           List<IRoomElement> newInv = this.model.getPlayer().getInventory().getItems().stream()
               .map(subType -> (IRoomElement) subType)
               .toList();
           this.view.getInventoryPanel().updatePanel(newInv);
+          }
         } catch (Exception error) {
           PopUp.confirmPopUp(error.getMessage());
         }
