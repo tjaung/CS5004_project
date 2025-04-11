4253e777c7f8b2fbb037199e40492169e13b85d0
tjaung <jaungt@gmail.com>
2025-04-11 17:59:26 -0400

    small bug fixes like item uses updating on failure and adding name input in beginning

--- AlignQuest/src/model/GameModel.java
+++ AlignQuest/src/model/GameModel.java
@@ -161,7 +161,6 @@ public class GameModel {
   public void useItem(Item item) {
     if (this.player.getInventory().hasItem(item)) {
       if (item.getUsesRemaining() > 0) {
-        item.setUsesRemaining(item.getUsesRemaining() - 1);
         Monster roomMonster = this.roomModel.getCurrentRoom().getMonster();
         Puzzle roomPuzzle = this.roomModel.getCurrentRoom().getPuzzle();
         Boolean isMonsterUsed = false;
@@ -180,6 +179,7 @@ public class GameModel {
           throw new IllegalArgumentException("There is nothing to use this on.\n");
         }
         else {
+          item.setUsesRemaining(item.getUsesRemaining() - 1);
           this.setString(this.roomModel.getCurrentRoom().toString());
         }
       } else {
